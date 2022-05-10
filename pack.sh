4a6e0e94e3596dc07631b56fa5f9cae044099ab5
Tomasz Krezymon <s189642@o365.student.pg.edu.pl>
2022-05-10 11:07:31 +0200

    master

--- pack.sh
+++ pack.sh
@@ -1,3 +1,7 @@
 packMenu(){
-    dialog --dselect . 20 20
+    PLIK=$(dialog --stdout --ok-button "DODAJ" --cancel-button "UTWORZ" --fselect ./ 0 0 0)	
+	EXIT=$?
+	if [[ -z $PLIK ]]; then
+		PLIK=$(pwd)
+	fi
 }
